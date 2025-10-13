a83c3c24affc3e08194366473db1e1a4d48574c2
Squadddd <ratsocho05@gmail.com>
2025-10-13 14:23:31 +0700

    feat: change login and register into components

--- ecommerce-frontend/src/App.jsx
+++ ecommerce-frontend/src/App.jsx
@@ -1,3 +1,8 @@
-function App() {}
+import Login from './components/Login.jsx';
+import Register from './components/Register.jsx';
+
+function App() {
+    return <Register/>
+}
 
 export default App;
