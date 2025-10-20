328c0d130586237f4ff85f46cb378e89a15a4ef1
nNm205 <minh2m5@gmail.com>
2025-10-20 13:03:39 +0700

    feat(ProductDetail): add ProductDetail Page

--- ecommerce-frontend/src/pages/ProductDetailPage.jsx
+++ ecommerce-frontend/src/pages/ProductDetailPage.jsx
@@ -1,5 +1,93 @@
+import ProductImages from "@/components/ProductImages";
+import ProductInfo from "@/components/ProductInfo";
+import ProductVariants from "@/components/ProductVariants";
+import AddToCartSection from "@/components/AddToCartSection";
+import ProductDescription from "@/components/ProductDescription";
+import ProductReviews from "@/components/ProductReviews";
+import RelatedProducts from "@/components/RelatedProducts";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
+import { useParams } from "react-router-dom";
+import { products } from "@/data/products";
+import { useState } from "react";
+
 function ProductDetailPage() {
-  return <div>Product Detail Page</div>;
+  const { id } = useParams();
+  const product = products.find((p) => p.id === id);
+  const [selected, setSelected] = useState({ size: null, color: null });
+
+  if (!product) {
+    return (
+      <div className="p-10 text-center text-gray-500">
+        Không tìm thấy sản phẩm
+      </div>
+    );
+  }
+
+  function handleAddToCart(quantity) {
+    alert(
+      `Đã thêm ${quantity} sản phẩm ${product.name} 
+      - ${selected.size || "?"} 
+      - ${selected.color || "?"} 
+      vào giỏ hàng!`
+    );
+  }
+
+  return (
+    <div className="w-full flex flex-col bg-white min-h-screen">
+      <Navbar />
+
+      <main
+        className="w-full flex flex-col 
+                    justify-center items-center 
+                    bg-white"
+      >
+        <div
+          className="w-full max-w-[1700px] 
+                      px-4 md:px-8 lg:px-12 
+                      py-12 grid md:grid-cols-2 
+                      gap-14"
+        >
+          <div className="flex items-start">
+            <ProductImages images={product.images} />
+          </div>
+
+          <div
+            className="flex flex-col justify-start 
+                        items-start"
+          >
+            <ProductInfo
+              name={product.name}
+              price={product.price}
+              rating={product.rating}
+              stock={product.stock}
+            />
+            <div className="mt-6 w-full">
+              <ProductVariants
+                sizes={product.sizes}
+                colors={product.colors}
+                selected={selected}
+                onChange={setSelected}
+              />
+              <AddToCartSection onAddToCart={handleAddToCart} />
+            </div>
+          </div>
+          <div className="col-span-2 mt-10">
+            <ProductDescription description={product.description} />
+            <ProductReviews reviews={product.reviews} />
+            <RelatedProducts
+              products={products.filter(
+                (p) => p.category === product.category && p.id != product.id
+              )}
+            />
+          </div>
+        </div>
+      </main>
+
+      <Footer />
+    </div>
+  );
 }
 
 export default ProductDetailPage;
