40a5858913256b1e164bd26bffbaf095d1b6bc78
Squadddd <ratsocho05@gmail.com>
2025-12-06 15:37:27 +0700

    abc

--- ecommerce-frontend/src/main.jsx
+++ ecommerce-frontend/src/main.jsx
@@ -2,15 +2,12 @@ import { StrictMode } from "react";
 import { BrowserRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./user/App.jsx";
-import { CartProvider } from "./user/contexts/CartContext";
+import App from "./admin/App.jsx";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
         <BrowserRouter>
-            <CartProvider>
-                <App />
-            </CartProvider>
+            <App />
         </BrowserRouter>
     </StrictMode>
 );
